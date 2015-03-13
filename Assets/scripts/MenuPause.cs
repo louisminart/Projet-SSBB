f4a97f01076e92245825121508bf5a7a9735d83f
proj445 <louisminart@MacBook-louis.local>
2015-03-13 18:03:05 +0100

    changements

--- Assets/scripts/MenuPause.cs
+++ Assets/scripts/MenuPause.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class MenuPause : MonoBehaviour {
 	#region Attributs
 	
-	private bool isPaused = false; // Permet de savoir si le jeu est en pause ou non.
+	private bool isPaused = false; 
 	
 	#endregion
 	
@@ -17,23 +17,23 @@ public class MenuPause : MonoBehaviour {
 	#region Methodes
 
 
-	// Use this for initialization
+
 	void Start () {
 	
 	}
 	
-	// Update is called once per frame
+
 	void Update () {
-		// Si le joueur appuis sur Echap alors la valeur de isPaused devient le contraire.
+
 		if(Input.GetKeyDown(KeyCode.Escape))
 			isPaused = !isPaused;
 		
 		
 		if(isPaused)
-			Time.timeScale = 0f; // Le temps s'arrete
+			Time.timeScale = 0f; 
 		
 		else
-			Time.timeScale = 1.0f; // Le temps reprend
+			Time.timeScale = 1.0f;
 		
 		
 
@@ -45,17 +45,21 @@ public class MenuPause : MonoBehaviour {
 		if(isPaused)
 		{
 			
-			// Si le bouton est présser alors isPaused devient faux donc le jeu reprend.
+
 			if(GUI.Button(new Rect(Screen.width / 2 - 40, Screen.height / 2 - 20, 80, 40), "Continuer"))
 			{
 				isPaused = false;
 			}
-			
-			// Si le bouton est présser alors on ferme completement le jeu ou charge la scene "Menu Principal
-			// Dans le cas du bouton quitter il faut augmenter sa postion Y pour qu'il soit plus bas
-			if(GUI.Button(new Rect(Screen.width / 2 - 40, Screen.height / 2 + 40, 80, 40), "Quitter"))
+			if(GUI.Button(new Rect(Screen.width / 2 - 40, Screen.height / 2 - 70, 80, 40), "Quitter"))
 			{
-				// Application.Quit(); 
+				Application.Quit(); 
+			}
+
+			
+
+			if(GUI.Button(new Rect(Screen.width / 2 - 40, Screen.height / 2 + 40, 80, 40), "Menu"))
+			{
+
 				Application.LoadLevel("Menu Principal"); 
 				
 			}
---
+++ Assets/scripts/Menuquitter.js
@@ -0,0 +1,16 @@
+﻿var texte;
+texte = GetComponentInChildren(MeshRenderer);
+ function OnMouseEnter () {
+ texte.material.color = Color.blue;
+}
+function OnMouseExit () {
+ texte.material.color = Color.white;
+}
+function OnMouseUp () {
+ Application.Quit();
+}
+
+function OnMouseDown () {
+
+ texte.material.color = Color.blue;
+}
